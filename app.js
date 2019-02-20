39427f8b97345eec8e21a426890cd1ac58400545
t-kowalewski <wkowalewski@outlook.com>
2019-02-20 22:49:48 +0100

    custom styles plus loader / results update

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 // Listen for submit button
-document.querySelector('#loan-form').addEventListener('submit', calculateResults);
+document.querySelector('#loan-form').addEventListener('submit', function (e) {
+    // Hide results (when you use calc once again)
+    document.querySelector('.results').style.display = 'none';
+
+    // Show Loader
+    document.querySelector('.loader').style.display = 'block';
+
+    // Set timer and start main func
+    setTimeout(calculateResults, 2000);
+
+    // Prevent default button beh.
+    e.preventDefault();
+});
 
 // Calculate Results - Function 
-function calculateResults(e) {
+function calculateResults() {
     // UI variables
     const amountUI = document.querySelector('#amount');
     const interestUI = document.querySelector('#interest');
@@ -11,7 +23,6 @@ function calculateResults(e) {
     const monthlyPaymentUI = document.querySelector('#monthly-payment');
     const totalPaymentUI = document.querySelector('#total-payment');
     const totalInterestUI = document.querySelector('#total-interest');
-
 
     const principal = parseFloat(amountUI.value);
     const calculatedInterest = parseFloat(interestUI.value) / 100 / 12;
@@ -25,31 +36,41 @@ function calculateResults(e) {
         monthlyPaymentUI.value = monthly.toFixed(2);
         totalPaymentUI.value = (monthly * calculatedPayments).toFixed(2);
         totalInterestUI.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+        // Hide loader
+        document.querySelector('.loader').style.display = 'none';
+        
+        // Show results
+        document.querySelector('.results').style.display = 'block';
     } else {
         showError('Please check your input');
     }
-
-    e.preventDefault();
 }
 
 // Show error - Function
 function showError(message) {
-    // Create a div
+    // Hide loader
+    document.querySelector('.loader').style.display = 'none'
+    // Hide results
+    document.querySelector('.results').style.display = 'none';
+
+
+    // Create a div for alert
     const errorDiv = document.createElement('div');
     // Get elements (where we heve to insert div)
     const card = document.querySelector('.card');
     const heading = document.querySelector('h1.heading');
-    
+
     // Add class
     errorDiv.className = 'alert alert-danger';
     // Add text node
     errorDiv.appendChild(document.createTextNode(message));
-    
+
     // Insert error div
     card.insertBefore(errorDiv, heading);
 
-    // Clear error after 3 second
-    setTimeout(clearError, 3000);
+    // Clear error after 3 seconds
+    setTimeout(clearError, 3500);
 }
 
 // Clear error - Function
